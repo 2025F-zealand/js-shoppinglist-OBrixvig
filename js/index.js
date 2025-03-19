470d2b209fff4bebc04beff126e5d4d3ae113c43
Oliver Colding Brixvig <olb005@edu.zealand.dk>
2025-03-19 10:30:07 +0100

    Ok

--- js/index.js
+++ js/index.js
@@ -1,2 +1,26 @@
 //Write your Javascript code here
 console.log("Shoppinglist")
+
+// Opgave 1
+function createNewLi(text){
+    let newLi = document.createElement("li");
+
+    newLi.setAttribute("id", "list");
+
+    newLi.setAttribute("class", "healthy");
+
+    const textNode = document.createTextNode(text);
+    newLi.appendChild(textNode);
+
+    return newLi;
+}
+
+// Opgave 2
+function AddLiToElement(){
+    
+}
+
+
+
+
+
